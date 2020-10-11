7a8fbb04ea6528092a38420125230b5df00afdd2
michael <michael@n.c>
2020-10-11 20:32:04 +0000

    excluded obsolete deps

--- src/main/java/sample/AsciiDocApplication.java
+++ src/main/java/sample/AsciiDocApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AsciiDocApplication {
 	public static void main(String[] args) {
-		FontInstaller.installFont(
-			AsciiDocApplication.class.getResource("documentation/assets/fonts/fg-virgil.ttf"));
+//		FontInstaller.installFont(
+//			AsciiDocApplication.class.getResource("documentation/assets/fonts/fg-virgil.ttf"));
 		SpringApplication.run(AsciiDocApplication.class, args);
 	}
 }
